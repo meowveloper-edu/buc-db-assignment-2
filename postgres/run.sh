8afa9322119eeb1839bb4f42010cbe6cf59ae3a5
meowveloper-edu <meowveloper.edu@gmail.com>
2025-08-02 03:16:08 +0700

    fix db name and data storage

--- postgres/run.sh
+++ postgres/run.sh
@@ -4,4 +4,4 @@
 SCRIPT_DIR=$(dirname "$0")
 
 # Run the psql command using a path relative to the script's directory
-psql -h postgres -U user -d user -f "${SCRIPT_DIR}/queries.sql"
+psql -h postgres -U user -d git_repo_db -f "${SCRIPT_DIR}/queries.sql"
